1bfac960680eba25f4f945c5214d2ac5e90fe3d1
Azim <budazimbud@gmail.com>
2023-06-05 10:35:46 +0700

    create and list users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 function App() {
+  const [users, setUsers] = useState([]);
+  const [userCreate, setUserCreate] = useState({
+    name: "",
+    email: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users")
+      .then((res) => {
+        console.log(res);
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3000/users", userCreate)
+      .then((res) => {
+        alert("success");
+      })
+      .catch((err) => {
+        alert(JSON.stringify(err));
+      });
+  };
+
+  const onChange = (event) => {
+    setUserCreate({ [event.target.name]: event.target.value });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((item, idx) => (
+        <h1 key={idx}>{item.email}</h1>
+      ))}
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="name" name="name" onChange={onChange} />
+        <input
+          type="text"
+          placeholder="email"
+          name="email"
+          onChange={onChange}
+        />
+        <button>Simpan</button>
+      </form>
     </div>
   );
 }
